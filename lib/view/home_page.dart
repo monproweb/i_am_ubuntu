0a03a1ea5b6d77612437011aac6175a34ba55d36
Thomas Erhel <thomas.erhel@gmail.com>
2021-09-25 01:35:53 +0200

    First commit

---
+++ lib/view/home_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.themeChanged}) : super(key: key);
+
+  final void Function(String themeName) themeChanged;
+
+  @override
+  // ignore: no_logic_in_create_state
+  _HomePageState createState() => _HomePageState(themeChanged);
+}
+
+class _HomePageState extends State<HomePage> {
+  _HomePageState(this.themeChanged);
+
+  var themeName = 'Yaru-light';
+  final void Function(String themeName) themeChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('I Am Ubuntu'),
+        leading: TextButton(
+          child: Icon(themeName.contains('-light')
+              ? Icons.dark_mode
+              : Icons.light_mode),
+          onPressed: () => {
+            setState(() {
+              if (themeName.contains('-light')) {
+                themeName = 'Yaru-dark';
+              } else if (themeName.contains('-dark')) {
+                themeName = 'Yaru-light';
+              }
+              themeChanged(themeName);
+            })
+          },
+        ),
+      ),
+      body: const Center(
+        child: Image(
+          image: AssetImage('images/logo_ubuntu.png'),
+        ),
+      ),
+    );
+  }
+}
